ad1497f1e8fc31db48691abc1f1e37731129d67b
naon708 <naoya_nis@icloud.com>
2023-03-23 10:03:00 +0900

    ミドルウェアの動作順を指定した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,8 @@ require "middlewares/upcase_middleware"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Rackミドルウェアを追加する
-  config.middleware.use UpcaseMiddleware
+  # config.middleware.use UpcaseMiddleware
+  config.middleware.insert_after Rack::ETag, UpcaseMiddleware
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
